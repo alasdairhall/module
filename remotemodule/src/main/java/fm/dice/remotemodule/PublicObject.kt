10e4f871d9974bdf77f8e2b2ca36ae251b3f429d
Alasdair Hall <al@dhsound.co.uk>
2022-10-11 15:37:46 +0100

    Snapshot take 3

--- remotemodule/src/main/java/fm/dice/remotemodule/PublicObject.kt
+++ remotemodule/src/main/java/fm/dice/remotemodule/PublicObject.kt
@@ -1,5 +1,5 @@
 package fm.dice.remotemodule
 
 object PublicObject {
-    val property = "You accessed this property: 0.0.6-SNAPSHOT take 2"
+    val property = "You accessed this property: 0.0.6-SNAPSHOT take 3"
 }
